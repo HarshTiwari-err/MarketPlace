e36f4a127763676567066affe791973c70f3e6cc
HarshTiwari-err <tiwari07.harsh@gmail.com>
2024-02-07 18:01:57 +0530

    Frontend: Product fetching/listing

--- src/components/ProductReel.tsx
+++ src/components/ProductReel.tsx
@@ -3,6 +3,7 @@ import { Product } from "../payload-types";
 import { TQueryValidator } from "../lib/validators/query-validator";
 import { trpc } from "@/trpc/client";
 import Link from "next/link";
+import ProductListing from "./ProductListing";
 
 interface ProductReelProps {
   title: string;
@@ -59,13 +60,13 @@ const ProductReel = (props: ProductReelProps) => {
         <div className="mt-6 flex items-center w-full">
           <div className="w-full grid grid-cols-2 gap-x-4 gap-y-10 sm:gap-x-6 md:grid-cols-4 md:gap-y-10 lg:gap-x-8">
             {
-            // map.map((product, i) => (
-              // <ProductListing
-              //   key={`product-${i}`}
-              //   product={product}
-              //   index={i}
-              // />
-            // ))
+            map.map((product, i) => (
+              <ProductListing
+                key={`product-${i}`}
+                product={product}
+                index={i}
+              />
+            ))
             }
           </div>
         </div>
