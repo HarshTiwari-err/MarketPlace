d1d7dd78cf686514c7a2c72144768ac2a2a32825
HarshTiwari-err <tiwari07.harsh@gmail.com>
2024-02-08 14:06:08 +0530

    Frontend: Airbnb Image Slider

--- src/components/ProductListing.tsx
+++ src/components/ProductListing.tsx
@@ -1,16 +1,21 @@
 "use client";
+
+import { Product } from "@/payload-types";
 import { useEffect, useState } from "react";
-import { Product } from "../payload-types";
 import { Skeleton } from "./ui/skeleton";
 import Link from "next/link";
 import { cn, formatPrice } from "@/lib/utils";
 import { PRODUCT_CATEGORIES } from "@/config";
+import ImageSlider from "./ImageSlider";
+
 interface ProductListingProps {
   product: Product | null;
   index: number;
 }
+
 const ProductListing = ({ product, index }: ProductListingProps) => {
-  const [isVisible, setIsVisible] = useState(false);
+  const [isVisible, setIsVisible] = useState<boolean>(false);
+
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsVisible(true);
@@ -24,27 +29,35 @@ const ProductListing = ({ product, index }: ProductListingProps) => {
   const label = PRODUCT_CATEGORIES.find(
     ({ value }) => value === product.category
   )?.label;
-  if (isVisible) {
+
+  const validUrls = product.images
+    .map(({ image }) => (typeof image === "string" ? image : image.url))
+    .filter(Boolean) as string[];
+
+  if (isVisible && product) {
     return (
       <Link
         className={cn("invisible h-full w-full cursor-pointer group/main", {
           "visible animate-in fade-in-5": isVisible,
         })}
-        href={`/product/product.id`}
+        href={`/product/${product.id}`}
       >
-        <h3 className='mt-4 font-medium text-sm text-gray-700'>
+        <div className="flex flex-col w-full">
+          <ImageSlider urls={validUrls} />
+
+          <h3 className="mt-4 font-medium text-sm text-gray-700">
             {product.name}
           </h3>
-          <p className='mt-1 text-sm text-gray-500'>
-            {label}
-          </p>
-          <p className='mt-1 font-medium text-sm text-gray-900'>
+          <p className="mt-1 text-sm text-gray-500">{label}</p>
+          <p className="mt-1 font-medium text-sm text-gray-900">
             {formatPrice(product.price)}
           </p>
+        </div>
       </Link>
     );
   }
 };
+
 const ProductPlaceholder = () => {
   return (
     <div className="flex flex-col w-full">
